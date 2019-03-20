2165bd2e68bbaea1db6dfb7f271d52345b59f7fd
Stephanie Hernandez <stephaniehernandez@stephanies-mbp.lan>
2019-03-19 20:22:22 -0600

    added action functionality to schedule course component to enable removal of course from schedule

--- src/components/schedule/scheduleCourse.js
+++ src/components/schedule/scheduleCourse.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions';
+import Action from '../action';
 
 class scheduleCourse extends Component {
     render() {
         return (
             <div className="schedule-course">
-                <label>{this.props.title}</label>
+                <label className="schedule-course__lbel">{this.props.title}</label>
+                <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="schedule-course__action" />
             </div>
         );
     }
 }
 
-export default scheduleCourse;
+export default connect(null, actions)(scheduleCourse);
